b95a3feaeba9fe0cdd4c0063003711dd65742462
elena <ikonnikovaev@gmail.com>
2022-11-09 12:20:18 +0300

    Stage 3: hiding and showing the message

--- Steganography and Cryptography/task/src/cryptography/Main.kt
+++ Steganography and Cryptography/task/src/cryptography/Main.kt
@@ -1,10 +1,14 @@
 package cryptography
 
+import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.File
+import java.io.FileNotFoundException
 import javax.imageio.ImageIO
 import java.lang.Exception
+import java.security.MessageDigest
 
+const val BITS_IN_BYTE = 8
 
 fun modifyImage(image: BufferedImage) {
     for (x in 0 until image.width) {
@@ -16,25 +20,93 @@ fun modifyImage(image: BufferedImage) {
     }
 }
 
+fun getBit(n: Int, pos: Int): Int {
+    return (n shr (BITS_IN_BYTE - 1 - pos)) and 1
+}
+
+fun hideMessageInImage(message: String, image: BufferedImage) {
+
+    val bytes = message.encodeToByteArray() + byteArrayOf(0, 0, 3)
+    val nBits = BITS_IN_BYTE * bytes.size
+    if (nBits > image.width * image.height) {
+        throw Exception("The input image is not large enough to hold this message.")
+    }
+    for (i in 0 until nBits) {
+        val x = i % image.width
+        val y = i / image.width
+        val q = i / BITS_IN_BYTE  // index of byte in array
+        val r = i % BITS_IN_BYTE  // index of bit in byte
+        val b = getBit(bytes[q].toInt(), r)
+        val color = image.getRGB(x, y)
+        val newColor = (color shr 1 shl 1) or b
+        // println(color)
+        // println(newColor)
+        image.setRGB(x, y, newColor)
+    }
+}
+
+fun readMessageFromImage(image: BufferedImage): String {
+    val endPattern = listOf("00000000".toUByte(2), "00000000".toUByte(2), "00000011".toUByte(2))
+    var bits = ""
+    var messageList = ubyteArrayOf()
+
+    for (y in 0 until image.height) {
+        for (x in 0 until image.width) {
+            if (bits.length == BITS_IN_BYTE) {
+                messageList += bits.toUByte(2)
+                bits = ""
+                if (messageList.size >= 3) {
+                    val index = messageList.lastIndex
+                    val lastThreeBytes = listOf(messageList[index - 2], messageList[index - 1], messageList[index])
+                    if (lastThreeBytes == endPattern) {
+                            val message = messageList.dropLast(3).toUByteArray().toByteArray()
+                            return message.toString(Charsets.UTF_8)}
+                }
+            }
+            val color = image.getRGB(x, y)
+            bits += (color and 1).toString()
+        }
+    }
+    val message = messageList.dropLast(3).toUByteArray().toByteArray()
+    return message.toString(Charsets.UTF_8)
+
+}
+
 fun hide() {
     println("Input image file:")
     val inputName = readln()
     println("Output image file:")
     val outputName = readln()
+    println("Message to hide:")
+    val message = readln()
     val inputFile = File(inputName)
     try {
         val image: BufferedImage = ImageIO.read(inputFile)
-        modifyImage(image)
+        // modifyImage(image)
+        hideMessageInImage(message, image)
         val outputFile = File(outputName)
         ImageIO.write(image, "png", outputFile)
-        println("Input Image: $inputName\n" +
-                "Output Image: $outputName\n" +
-                "Image $outputName is saved."
-        )
+        println("Message saved in $outputName image.")
+    } catch (e: FileNotFoundException) {
+        println("Can't read input file!")
     } catch (e: Exception) {
+        println(e.toString())
+    }
+}
+
+fun show() {
+    println("Input image file:")
+    val inputName = readln()
+    val inputFile = File(inputName)
+    try {
+        val image: BufferedImage = ImageIO.read(inputFile)
+        val message = readMessageFromImage(image)
+        println("Message:")
+        println(message)
+
+    } catch (e: FileNotFoundException) {
         println("Can't read input file!")
     }
-
 }
 
 
@@ -52,7 +124,8 @@ fun chooseTask() {
                 hide()
             }
             "show" -> {
-                println("Obtaining message from image.")
+                // println("Obtaining message from image.")
+                show()
             }
             else -> {
                 println("Wrong task: [input String]")
@@ -62,6 +135,7 @@ fun chooseTask() {
 }
 
 fun main() {
+
     chooseTask()
 }
 
